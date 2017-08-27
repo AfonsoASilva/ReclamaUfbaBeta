251d3380570cc391dbe101c5ed57b89d76e7e1fc
Deuana <deuana.telles@hotmail.com>
2017-08-27 01:15:23 -0300

    Criação do usuário

---
+++ app/src/main/java/br/com/reclamaufbabeta/Controller/UsuarioController.java
@@ -0,0 +1,43 @@
+package br.com.reclamaufbabeta.Controller;
+
+import android.content.Context;
+
+import java.util.List;
+
+import br.com.reclamaufbabeta.DAO.UsuarioDAO;
+import br.com.reclamaufbabeta.modelo.Usuario;
+
+public class UsuarioController {
+    private static UsuarioDAO usuarioDAO;
+    private static UsuarioController instance;
+
+    public static UsuarioController getInstance(Context context) {
+        if (instance == null) {
+            instance = new UsuarioController();
+            usuarioDAO = new UsuarioDAO(context);
+        }
+        return instance;
+    }
+    public void insert(Usuario usuario) throws Exception {
+        usuarioDAO.insert(usuario);
+    }
+    public void update(Usuario usuario) throws Exception {
+        usuarioDAO.update(usuario);
+    }
+    public List<Usuario> findAll() throws Exception {
+        return usuarioDAO.findAll();
+    }
+
+    public boolean validacao(String login, String senha) throws Exception {
+        Usuario usuario = usuarioDAO.findByLogin(login, senha);
+        if (usuario == null || usuario.getUsuario() == null || usuario.getSenha() == null) {
+            return false;
+        }
+        String informado = login + senha;
+        String esperado = usuario.getUsuario() + usuario.getSenha();
+        if (informado.equals(esperado)) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/br/com/reclamaufbabeta/LoginActivity.java
@@ -0,0 +1,63 @@
+package br.com.reclamaufbabeta;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import br.com.reclamaufbabeta.Controller.UsuarioController;
+import br.com.reclamaufbabeta.modelo.Usuario;
+
+
+public class LoginActivity extends Activity {
+    private EditText campoUsuario, campoSenha;
+    private Context context;
+    private UsuarioController usuarioController;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        context = this;
+        usuarioController = UsuarioController.getInstance(context);
+        campoUsuario = (EditText) findViewById(R.id.login_usuario);
+        campoSenha = (EditText) findViewById(R.id.login_senha);
+
+        Usuario usuario = new Usuario(1, "adm", "adm");
+        try {
+            usuarioController.insert(usuario);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Button entrarBtn = (Button) findViewById(R.id.login_entrar);
+        entrarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentListaPosts = new Intent(LoginActivity.this, ListaPostsActivity.class);
+                startActivity(intentListaPosts);
+            }
+        });
+    }
+
+    public void validar(View view) {
+        String usuario = campoUsuario.getText().toString();
+        String senha = campoSenha.getText().toString();
+        try {
+            boolean isValid = usuarioController.validacao(usuario, senha);
+            if (isValid) {
+                Toast.makeText(LoginActivity.this, "Usuario e senha validados com sucesso!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(LoginActivity.this,"Verifique usuario e senha!", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(LoginActivity.this,"Erro validando usuario e senha", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+}
